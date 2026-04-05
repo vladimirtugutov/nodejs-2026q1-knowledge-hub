85a41c674ec00e4476eafde8f7a632ee4d8bf8d3
Vladimir Tugutov <tugutov@gmail.com>
2026-04-05 09:14:37 -0300

    feat: add main, app.module, enums

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { ArticleModule } from './article/article.module';
+import { CategoryModule } from './category/category.module';
+import { CommentModule } from './comment/comment.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UserModule, ArticleModule, CategoryModule, CommentModule],
 })
 export class AppModule {}
