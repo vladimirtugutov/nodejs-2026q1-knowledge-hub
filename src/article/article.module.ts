c258c41d9bb0c7fcf4ab0aa9c5e9c9b185b332f0
Vladimir Tugutov <tugutov@gmail.com>
2026-04-05 09:30:42 -0300

    feat: add article, category, comment, user blank modules

---
+++ src/article/article.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { ArticleController } from './article.controller';
+import { ArticleService } from './article.service';
+import { ArticleRepository } from './article.repository';
+
+@Module({
+  controllers: [ArticleController],
+  providers: [ArticleService, ArticleRepository],
+  exports: [ArticleService, ArticleRepository],
+})
+export class ArticleModule {}
